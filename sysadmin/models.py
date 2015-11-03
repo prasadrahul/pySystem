4c4d59962e804a157ed69312bfce185c844bcb23
rahul prasad <prasad_rahul@hotmail.co.in>
2015-11-03 20:23:35 +0530

    pySystem: Adding model string representations in model class

--- sysadmin/models.py
+++ sysadmin/models.py
@@ -5,6 +5,9 @@ class Logintype(models.Model):
     login_id = models.CharField(max_length=10)
     login_type_name = models.CharField(max_length=30)
     login_type = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.login_type_name
 
 class Loginmanagement(models.Model):
     login_name = models.CharField(max_length=30)
@@ -17,6 +20,7 @@ class Loginmanagement(models.Model):
     state_province = models.CharField(max_length=30)
     country = models.CharField(max_length=50)
     create_date = models.DateTimeField(auto_now_add=True, blank=True)
-
-
+    
+    def __str__(self):
+        return '%s %s %s' % (self.login_name, self.first_name, self.last_name)
 
