e2c7b552b2bfe89c53505d38c98590f3069cf10d
rahul prasad <prasad_rahul@hotmail.co.in>
2015-11-05 18:50:58 +0530

    pySystem: Adding Apache-2 log viewer functions

--- pySystem/urls.py
+++ pySystem/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from sysadmin.views import current_datetime, login_index, register_index, register_new_user
+from logview.views import *
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,8 +10,11 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^time/$', current_datetime ),
-    url(r'^$', login_index),
+    #url(r'^$', login_index),
     url(r'^register/$', register_index ),
     url(r'^register/$', register_new_user),
+
+    url(r'^$', 'pySystem.logview.views.list_files'),
+    url(r'^viewlog/(?P<sortmethod>.*?)/(?P<filename>.*?)/$','pySystem.logview.views.view_log'),
 
 )
